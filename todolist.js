5a4e600e000a34ffeba625e028520eda0e26a116
Balem Régine <74600487+regine-balem@users.noreply.github.com>
2021-01-19 15:28:56 +0100

    Add files via upload

---
+++ todolist.js
@@ -0,0 +1,41 @@
+
+const elementTODOLIST = document.getElementById("todolist");
+let todo = document.getElementById("todo");
+
+function ajoutTodo() {
+
+    let data = {
+        description: todo.value
+    }
+    fetch('http://balem1393.pythonanywhere.com/todo', {
+        method: 'POST',
+        headers: {
+            "Content-type": 'application/json'
+        },
+        body: JSON.stringify(data),
+    }).then(response => rafraichirHTML())
+}
+
+function deleteTodo(indice) {
+    fetch('http://balem1393.pythonanywhere.com/todo/' + indice, {
+        method: 'DELETE'})
+        .then(response => rafraichirHTML())
+}
+
+function rafraichirHTML() {
+    todo.value = "";
+    fetch('http://balem1393.pythonanywhere.com/todo/')
+        .then(response => response.json())
+        .then(todolist => {
+            elementTODOLIST.innerHTML= "";
+            for (indice in todolist) {
+                let li = document.createElement("li");
+                li.innerHTML = 
+                "<span>" + todolist[indice].description + "</span>&nbsp;" +
+                "<a class='deleteBtn' onclick='deleteTodo(" + indice + ")'>&#10006;</a>";
+                elementTODOLIST.appendChild(li);
+            }
+        });
+}
+
+rafraichirHTML()
